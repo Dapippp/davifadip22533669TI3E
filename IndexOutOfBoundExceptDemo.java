7c0deaf3306875c55f88ed6769a4f57eb9ebcc08
Davif Adi Prayogo <88437267+Dapippp@users.noreply.github.com>
2024-01-23 17:35:04 +0700

    Update IndexOutOfBoundExceptDemo.java

--- IndexOutOfBoundExceptDemo.java
+++ IndexOutOfBoundExceptDemo.java
@@ -1,7 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptiondavif;
 
+/**
+ *
+ * @author USER
+ */
 public class IndexOutOfBoundExceptDemo {
-    int[] numbers = { 1, 2, 3, 4, 5 };
-
+    int[] numbers = {1, 2, 3, 4, 5};
+    
     public static void main(String[] args) {
         IndexOutOfBoundExceptDemo arr1 = new IndexOutOfBoundExceptDemo();
         try {
@@ -11,6 +20,6 @@ public class IndexOutOfBoundExceptDemo {
             System.out.println("Gagal mendapatkan nilai x");
             System.out.println(e.getMessage());
         }
-
+        
     }
 }
